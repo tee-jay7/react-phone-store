3b7b35758efdf03f44459082e375a32e7168f5f0
John <twasam.john@gmail.com>
2020-07-21 12:56:19 +0100

    Setup the page not found component

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Title from "./Title";
+import { Link } from "react-router-dom";
+import notfound from "../notfound.svg";
 
 function Default() {
-  return <div>Hello from Page not found</div>;
+  return (
+    <div>
+      <Title name="page not" title="found" />
+      <Link to="/">
+        <p className="mx-auto text-center text-title text-uppercase">
+          head back home
+        </p>
+      </Link>
+      <img
+        src={notfound}
+        alt="page not found"
+        className="img-fluid container-fluid"
+        style={{ width: "vw", height: "400px" }}
+      />
+    </div>
+  );
 }
 
 export default Default;
